31876cb40c02c9706d7bd56bc6d5c81d09e0f4b2
unknown <baguswidiat42@gmail.com>
2024-12-03 08:00:50 +0700

    day 2 ngebuat feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from './components/Hero';
 import About from './components/About';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Feature from './components/Feature';
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden ">
       <Navbar />
       <Hero />
       <About />
+      <Feature />
     </main>
   )
 }
